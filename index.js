2003f6ace08ed54bd0b8a4e8d7c793c18f8be83a
s.beriaghdar <fried.code@gmail.com>
2015-09-12 11:14:42 +0430

    trying to fix subpixel render - adding fluid layouts

--- index.js
+++ index.js
@@ -30,12 +30,25 @@ var server = app.listen(3000, function () {
     .viewport(480, 1800)
     .wait()
     .screenshot('grids/xs.png')
+    .viewport(1200, 1800)
+    .goto('http:/localhost:3000/fluid.html')
+    .wait()
+    .screenshot('grids/lg-fluid.png')
+    .viewport(992, 1800)
+    .wait()
+    .screenshot('grids/md-fluid.png')
+    .viewport(768, 1800)
+    .wait()
+    .screenshot('grids/sm-fluid.png')
+    .viewport(480, 1800)
+    .wait()
+    .screenshot('grids/xs-fluid.png')
     .run(function(err, nightmare){
+      server.close();
       if(err){
         console.error(err);
       }else{
         console.log('done!');
-        server.close();
         gm("grids/xs.png").transparent('#FFFFFF').write('grids/xs.png', function (err) {
           if (err) {
             console.error(err);
@@ -52,6 +65,26 @@ var server = app.listen(3000, function () {
           }
         });
         gm("grids/lg.png").transparent('#FFFFFF').write('grids/lg.png', function (err) {
+          if (err) {
+            console.error(err);
+          }
+        });
+        gm("grids/xs-fluid.png").transparent('#FFFFFF').write('grids/xs-fluid.png', function (err) {
+          if (err) {
+            console.error(err);
+          }
+        });
+        gm("grids/sm-fluid.png").transparent('#FFFFFF').write('grids/sm-fluid.png', function (err) {
+          if (err) {
+            console.error(err);
+          }
+        });
+        gm("grids/md-fluid.png").transparent('#FFFFFF').write('grids/md-fluid.png', function (err) {
+          if (err) {
+            console.error(err);
+          }
+        });
+        gm("grids/lg-fluid.png").transparent('#FFFFFF').write('grids/lg-fluid.png', function (err) {
           if (err) {
             console.error(err);
           }
